f3fd3c2131682d25a2abefb53db37831e6b93640
gabraua <gabrielcmarinho@hotmail.com>
2024-11-13 10:32:55 -0300

    adicionado o form

--- Estudando/src/main/java/com/primeFaces/Estudando/EstudandoApplication.java
+++ Estudando/src/main/java/com/primeFaces/Estudando/EstudandoApplication.java
@@ -8,6 +8,8 @@ public class EstudandoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudandoApplication.class, args);
+
 	}
+
 
 }
--- Estudando/src/main/java/com/primeFaces/Estudando/modelo/Aluno.java
+++ Estudando/src/main/java/com/primeFaces/Estudando/modelo/Aluno.java
@@ -2,8 +2,9 @@ package com.primeFaces.Estudando.modelo;
 
 import java.time.Instant;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
